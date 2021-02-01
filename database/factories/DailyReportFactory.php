fb1b716d08205699c7dd2ee695000d7c2567dc25
daikke <amorer2@gmail.com>
2021-02-01 18:26:14 +0900

    feature/GL-3:create daily_reports seeder

--- database/factories/DailyReportFactory.php
+++ database/factories/DailyReportFactory.php
@@ -1,9 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Models\User;
 
 $factory->define(App\Models\DailyReport::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => User::inRandomOrder()->first()->id,
+        'title' => $faker->word,
+        'contents' => $faker->text(255),
+        'reporting_time' => $faker->date($format = 'Y-m-d', $max = 'now'),
     ];
 });
