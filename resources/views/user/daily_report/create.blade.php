c9240e6d7820bdab75a8eb9ea9ef9c901f360125
daikke <amorer2@gmail.com>
2021-02-03 21:00:26 +0900

    feature/GL-3-1:作成画面のblade作成

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -4,18 +4,30 @@
 <h2 class="brand-header">日報作成</h2>
 <div class="main-wrap">
   <div class="container">
-      {!! Form::open(['route' => 'report.create']) !!}
-      <div class="form-group form-size-small has-error">
-        <input type="date" class="form-control">
-        <span class="help-block"></span>
+    {!! Form::open(['route' => 'report.create']) !!}
+      <div class="form-group form-size-small @if ($errors->has('reporting_time')) has-error @endif">
+        {!! Form::date('reporting_time', '', ['class' => 'form-control']) !!}
+        <span class="help-block">
+          @foreach ($errors->get('reporting_time') as $error)
+            {{ $error }}
+          @endforeach
+        </span>
       </div>
-      <div class="form-group has-error">
-        <input type="text" class="form-control" placeholder="Title">
-        <span class="help-block"></span>
+      <div class="form-group @if ($errors->has('title')) has-error @endif">
+        {!! Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title']) !!}
+        <span class="help-block">
+          @foreach ($errors->get('title') as $error)
+            {{ $error }}
+          @endforeach
+        </span>
       </div>
-      <div class="form-group has-error">
-        <textarea class="form-control" placeholder="Content"></textarea>
-        <span class="help-block"></span>
+      <div class="form-group @if ($errors->has('contents')) has-error @endif">
+        {!! Form::textarea('contents', '', ['class' => 'form-control', 'placeholder' => 'Content']) !!}
+        <span class="help-block">
+          @foreach ($errors->get('contents') as $error)
+            {{ $error }}
+          @endforeach
+        </span>
       </div>
       <button type="submit" class="btn btn-success pull-right">Add</button>
     {!! Form::close() !!}
