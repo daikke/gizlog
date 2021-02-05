77b5d94f3b1c892cbbd00d2313107d14a0386a64
daikke <amorer2@gmail.com>
2021-02-05 13:20:32 +0900

    feature/GL-3-1:シーディングされるデータのuser_idを定数指定

--- database/factories/DailyReportFactory.php
+++ database/factories/DailyReportFactory.php
@@ -2,10 +2,11 @@
 
 use Faker\Generator as Faker;
 use App\Models\User;
+const TEST_USER_IDS = [1, 2, 3, 4];
 
 $factory->define(App\Models\DailyReport::class, function (Faker $faker) {
     return [
-        'user_id' => User::inRandomOrder()->first()->id,
+        'user_id' => TEST_USER_IDS[array_rand(TEST_USER_IDS, 1)],
         'title' => $faker->word,
         'contents' => $faker->text(255),
         'reporting_time' => $faker->date($format = 'Y-m-d', $max = 'now'),
