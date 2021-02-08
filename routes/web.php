a23a5eaa8e8e7b0c758243b25dc26832216a6a25
daikke <amorer2@gmail.com>
2021-02-08 20:00:37 +0900

    feature/GL-3-2:routeをグループ化・詳細ルート設定

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,12 @@ Route::group(['prefix' => '/', 'user.', 'namespace' => 'User'], function () {
     Route::get('callback', 'Auth\AuthenticateController@loginBySlackUserInfo');
     Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-    Route::get('report', 'DailyReportController@index')->name('report.index');
-    Route::get('report/create', 'DailyReportController@create')->name('report.create');
-    Route::post('report', 'DailyReportController@store')->name('report.store');
+    Route::group(['prefix' => 'report', 'as' => 'report.'], function() {
+        Route::get('/', 'DailyReportController@index')->name('index');
+        Route::get('create', 'DailyReportController@create')->name('create');
+        Route::post('/', 'DailyReportController@store')->name('store');
+        Route::get('{id}', 'DailyReportController@show')->name('show');
+    });
 });
 
 // 管理者側画面
