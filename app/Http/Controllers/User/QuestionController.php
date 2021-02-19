6089ac91aea5d55d73d313bcc4ffa36c9502fbb3
daikke <amorer2@gmail.com>
2021-02-19 17:30:46 +0900

    feature/GL-4-1:indexアクション定義

--- app/Http/Controllers/User/QuestionController.php
+++ app/Http/Controllers/User/QuestionController.php
@@ -4,8 +4,28 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Question;
+use App\Models\TagCategory;
+use Illuminate\View\View;
 
 class QuestionController extends Controller
 {
-    //
+    private $question;
+
+    public function __construct(Question $question)
+    {
+        $this->question = $question;
+    }
+
+    /**
+     * 一覧表示
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        $questions = $this->question->all();
+        $tagCategories = TagCategory::all();
+        return view('user.question.index', compact('questions', 'tagCategories'));
+    }
 }
