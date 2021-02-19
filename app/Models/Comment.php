2bdb1d3cd1f9b0ece74ceb0b8de82a1241642dc9
daikke <amorer2@gmail.com>
2021-02-19 19:15:57 +0900

    feature/GL-4-2:commentテーブルリレーション定義

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
-    //
+    /**
+     * usersテーブルリレーション
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
