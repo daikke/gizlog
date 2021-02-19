d8b47d469b5e8dc5a1b16b659c4fa160a6f2952f
daikke <amorer2@gmail.com>
2021-02-19 19:27:27 +0900

    feature/GL-4-2:詳細画面表示機構作成

--- resources/views/user/question/show.blade.php
+++ resources/views/user/question/show.blade.php
@@ -6,38 +6,40 @@
   <div class="panel panel-success">
     <div class="panel-heading">
       <img src="" class="avatar-img">
-      <p>&nbsp;さんの質問&nbsp;&nbsp;(&nbsp;&nbsp;)</p>
-      <p class="question-date"></p>
+      <p>{{ $question->user->name }}さんの質問&nbsp;&nbsp;({{ $question->tagCategory->name }})</p>
+      <p class="question-date">{{ $question->created_at }}</p>
     </div>
     <div class="table-responsive">
       <table class="table table-striped table-bordered">
         <tbody>
           <tr>
             <th class="table-column">Title</th>
-            <td class="td-text"></td>
+            <td class="td-text">{{ $question->title }}</td>
           </tr>
           <tr>
             <th class="table-column">Question</th>
-            <td class='td-text'></td>
+            <td class='td-text'>{{ nl2br($question->content) }}</td>
           </tr>
         </tbody>
       </table>
     </div>
   </div>
     <div class="comment-list">
+      @foreach ($question->comments as $comment)
         <div class="comment-wrap">
           <div class="comment-title">
-            <img src="" class="avatar-img">
-            <p></p>
-            <p class="comment-date"></p>
+            <img src="{{ $comment->user->avatar }}" class="avatar-img">
+            <p>{{ $comment->title }}</p>
+            <p class="comment-date">{{ $comment->created_at }}</p>
           </div>
-          <div class="comment-body"></div>
+          <div class="comment-body">{{ nl2br($comment->content) }}</div>
         </div>
+      @endforeach
     </div>
   <div class="comment-box">
     <form>
-      <input name="user_id" type="hidden" value="">
-      <input name="question_id" type="hidden" value="">
+      <input name="user_id" type="hidden" value="{{ Auth::id() }}">
+      <input name="question_id" type="hidden" value="{{ $question->id }}">
       <div class="comment-title">
         <img src="" class="avatar-img"><p>コメントを投稿する</p>
       </div>
