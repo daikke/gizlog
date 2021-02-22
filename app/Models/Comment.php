0d72257f0ebaf144bce4309b801b5af6ff4868e4
daikke <amorer2@gmail.com>
2021-02-22 21:20:52 +0900

    feature/GL-4-5:リレーションモデル実装

---
+++ app/Models/Comment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * commentsテーブルのモデルクラス
+ */
+class Comment extends Model
+{
+    //
+}
---
+++ app/Models/TagCategory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * tag_categoriesテーブルのモデルクラス
+ */
+class TagCategory extends Model
+{
+    //
+}
