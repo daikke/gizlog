7084affc9d28350f833183c2cae64a41aaf61a5e
daikke <amorer2@gmail.com>
2021-02-22 21:02:24 +0900

    feature/GL-4-4:確認画面実装

--- app/Http/Controllers/User/QuestionController.php
+++ app/Http/Controllers/User/QuestionController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\QuestionsRequest;
 use App\Models\Question;
 use App\Models\TagCategory;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class QuestionController extends Controller
@@ -38,5 +41,31 @@ class QuestionController extends Controller
         $tagCategories = TagCategory::pluck('name', 'id');
         $question = $this->question->find($id);
         return view('user.question.edit', compact('question', 'tagCategories'));
+    }
+
+    /**
+     * 確認画面表示
+     *
+     * @param QuestionsRequest $request
+     * @return View
+     */
+    public function confirm(QuestionsRequest $request): View
+    {
+        $question = $request->all();
+        return view('user.question.confirm', compact('question'));
+    }
+
+    /**
+     * 質問作成
+     *
+     * @param QuestionsRequest $request
+     * @return RedirectResponse
+     */
+    public function store(QuestionsRequest $request): RedirectResponse
+    {
+        $input = $request->all();
+        $this->question->user_id = Auth::id();
+        $this->question->fill($input)->save();
+        return redirect()->route('question.mypage');
     }
 }
--- resources/views/user/question/confirm.blade.php
+++ resources/views/user/question/confirm.blade.php
@@ -5,33 +5,31 @@
 <div class="main-wrap">
   <div class="panel panel-success">
     <div class="panel-heading">
-      の質問
+      {{ Auth::user()->name }}の質問
     </div>
     <div class="table-responsive">
       <table class="table table-striped table-bordered">
         <tbody>
           <tr>
             <th class="table-column">Title</th>
-            <td class="td-text"></td>
+            <td class="td-text">{{ $question['title'] }}</td>
           </tr>
           <tr>
             <th class="table-column">Question</th>
-            <td class='td-text'></td>
+            <td class='td-text'>{!! nl2br($question['content']) !!}</td>
           </tr>
         </tbody>
       </table>
     </div>
   </div>
   <div class="btn-bottom-wrapper">
-    <form>
-      <input name="user_id" type="hidden" value="">
-      <input name="tag_category_id" type="hidden" value="">
-      <input name="title" type="hidden" value="">
-      <input name="content" type="hidden" value="">
+    {{ Form::open(['route' => 'question.store']) }}
+      <input name="tag_category_id" type="hidden" value="{{ $question['tag_category_id'] }}">
+      <input name="title" type="hidden" value="{{ $question['title'] }}">
+      <input name="content" type="hidden" value="{{ $question['content'] }}">
       <button type="submit" class="btn btn-success"><i class="fa fa-check" aria-hidden="true"></i></button>
-    </form>
+    {!! Form::close() !!}
   </div>
 </div>
 
 @endsection
-
