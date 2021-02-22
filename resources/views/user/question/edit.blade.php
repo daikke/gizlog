01901f934ff2ff68c4adbfb0dfcaed5cca9d070c
daikke <amorer2@gmail.com>
2021-02-22 22:02:00 +0900

    feature/GL-4-4:編集画面バリデーションエラー表示機構実装

--- resources/views/user/question/edit.blade.php
+++ resources/views/user/question/edit.blade.php
@@ -6,17 +6,23 @@
 <div class="main-wrap">
   <div class="container">
     {{ Form::open(['route' => ['question.confirm', $question->id], 'method' => 'POST']) }}
-      <div class="form-group">
+      <div class="form-group  @if($errors->has('tag_category_id')) has-error @endif">
         {!! Form::select('tag_category_id', $tagCategories, $question->tag_category_id, ['class' => 'form-control selectpicker form-size-small', 'id' => 'pref_id']) !!}
-        <span class="help-block"></span>
+        @foreach ($errors->get('tag_category_id') as $error)
+          <span class="help-block">{{ $error }}</span>
+        @endforeach
       </div>
-      <div class="form-group">
+      <div class="form-group @if($errors->has('title')) has-error @endif">
         {!! Form::text('title', $question->title, ['class' => 'form-control', 'placeholder' => 'title']) !!}
-        <span class="help-block"></span>
+        @foreach ($errors->get('title') as $error)
+          <span class="help-block">{{ $error }}</span>
+        @endforeach
       </div>
-      <div class="form-group">
+      <div class="form-group @if ($errors->has('content')) has-error @endif">
         {!! Form::textarea('content', $question->content, ['class' => 'form-control', 'placeholder' => 'Please write down your question here...']) !!}
-        <span class="help-block"></span>
+        @foreach ($errors->get('content') as $error)
+          <span class="help-block">{{ $error }}</span>
+        @endforeach
       </div>
       <input name="confirm" class="btn btn-success pull-right" type="submit" value="update">
     {{ Form::close() }}
