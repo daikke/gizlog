c04320f68236be3bcec97b9f8338c347ebc87ad3
daikke <amorer2@gmail.com>
2021-03-01 18:39:50 +0900

    feature/GL-5-1:モデル・コントローラー定義

---
+++ app/Models/Book.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Book extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = [
+        'reporting_time',
+        'deleted_at',
+    ];
+}
