439ef12416265b61a379676c6a32a4db1113c6cb
daikke <amorer2@gmail.com>
2021-03-10 12:52:02 +0900

    feature/GL-4-3:SelectCategoryの値をセット

--- app/Http/Controllers/User/QuestionController.php
+++ app/Http/Controllers/User/QuestionController.php
@@ -54,7 +54,7 @@ class QuestionController extends Controller
     public function create(): View
     {
         $tagCategories = TagCategory::pluck('name', 'id');
-        $tagCategories->prepend('Select category');
+        $tagCategories->prepend('Select category', '');
         return view('user.question.create', compact('tagCategories'));
     }
 
