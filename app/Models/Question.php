be0104c4d1339bf0e50ee7807ae35baaea05a6d7
daikke <amorer2@gmail.com>
2021-03-11 17:45:01 +0900

    [fix]リレーション定義

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -68,11 +69,11 @@ class Question extends Model
     /**
      * tag_categoriesテーブルリレーション
      *
-     * @return BelongsTo
+     * @return belongsToMany
      */
-    public function tagCategory(): BelongsTo
+    public function tagCategories(): BelongsToMany
     {
-        return $this->belongsTo(TagCategory::class);
+        return $this->belongsToMany(TagCategory::class);
     }
 
     /**
