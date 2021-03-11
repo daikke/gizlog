26fc7f071b4c1a0ab7b033a08ffb5502c3423345
daikke <amorer2@gmail.com>
2021-03-11 17:39:54 +0900

    [add]テーブル定義

---
+++ database/migrations/2021_03_10_190432_create_question_tag_category_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionTagCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_tag_category', function (Blueprint $table) {
+            $table->integer('question_id')->unsigned();
+            $table->integer('tag_category_id')->unsigned();
+            $table->primary(['question_id', 'tag_category_id']);
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+            $table->foreign('tag_category_id')->references('id')->on('tag_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question_tag_category');
+    }
+}
