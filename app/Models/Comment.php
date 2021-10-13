7e58b3329a53b57e7629250349d2cb5dfe65a541
阿部大祐 <abe@gizumo-inc.jp>
2021-10-13 12:44:55 +0900

    add:ユーザーごとのコメント数ランキング取得メソッド追加

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 /**
  * commentsテーブルのモデルクラス
@@ -26,5 +28,24 @@ class Comment extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\Models\User');
+    }
+
+    /**
+     * ユーザーごとのコメント数ランキング取得
+     *
+     * @return Collection
+     */
+    public function fetchUserCommentsCountsRankings(): Collection
+    {
+        $selectColumns = [
+            'user_id',
+            'COUNT(*) AS comments_count',
+        ];
+
+        return $this
+            ->select(DB::raw(implode(',', $selectColumns)))
+            ->groupBy('user_id')
+            ->orderByDesc('comments_count')
+            ->get();
     }
 }
