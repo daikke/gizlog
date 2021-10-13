6496b84063b3cd3872d8340740a7388c8f3ea3c8
阿部大祐 <abe@gizumo-inc.jp>
2021-10-13 14:03:41 +0900

    add:ランキングセレクトコンポーネントの追加

--- resources/views/user/question/ranking/question.blade.php
+++ resources/views/user/question/ranking/question.blade.php
@@ -4,6 +4,7 @@
 <h2 class="brand-header">ランキング&nbsp;「質問数が多いユーザー」</h2>
 <div class="main-wrap">
   <div class="content-wrapper table-responsive">
+    @include('user.question.components.select_ranking')
     <table class="table table-striped">
       <thead>
         <tr class="rows">
