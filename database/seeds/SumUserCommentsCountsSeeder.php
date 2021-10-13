12c6c28bc64d8bcf5eb42e3bfeaf7323b427351e
阿部大祐 <abe@gizumo-inc.jp>
2021-10-13 12:31:49 +0900

    add:userごとのコメントランキング追加

--- database/seeds/SumUserCommentsCountsSeeder.php
+++ database/seeds/SumUserCommentsCountsSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
-class SumUserCommentsCountsSeeder extends Seeder
+class SummaryUserCommentsCountsRankingsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +12,13 @@ class SumUserCommentsCountsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comment = app()->make(Comment::class);
+        $rankings = $comment->fetchUserCommentsCountsRankings();
+        $rankings->map(function ($row, $key) {
+            return $row['rank'] = $key + 1;
+        });
+
+        DB::table('summary_user_comments_counts_rankings')->truncate();
+        DB::table('summary_user_comments_counts_rankings')->insert($rankings->toArray());
     }
 }
